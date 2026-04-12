84a6b5d33d08dc29566a993d5034b11ebde35e08
shomy <git@itssho.my>
2026-04-12 03:45:10 +0200

    core: Re-export common types in lib.rs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -12,9 +12,24 @@ pub mod exploit;
 pub mod macros;
 pub mod utilities;
 
+pub use core::auth::{AuthManager, SignData, SignPurpose, SignRequest, Signer};
 pub use core::log_buffer::{DeviceLog, OnPush};
+pub use core::seccfg::LockFlag;
+pub use core::storage::{
+    EmmcPartition,
+    Gpt,
+    Partition,
+    PartitionKind,
+    RpmbRegion,
+    Storage,
+    StorageKind,
+    StorageType,
+    UfsPartition,
+};
 
 pub use connection::port::{MTKPort, find_mtk_port};
+pub use da::protocol::{BootMode, DAProtocol, DownloadProtocol};
+pub use da::{DA, DAEntryRegion, DAFile, DAType, XFlash, Xml};
 pub use device::{Device, DeviceBuilder};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
