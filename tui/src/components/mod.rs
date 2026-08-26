47ea82c655f5ec87bb04cdeb937daf5631d93404
shomy <git@itssho.my>
2026-08-26 15:03:42 +0200

    tui: Add some more components
    Signed-off-by: shomy <git@itssho.my>

--- tui/src/components/mod.rs
+++ tui/src/components/mod.rs
@@ -2,25 +2,37 @@
     SPDX-License-Identifier: AGPL-3.0-or-later
     SPDX-FileCopyrightText: 2025-2026 Shomy
 */
+pub mod activity_indicator;
 pub mod blinking_stars;
 pub mod card_view;
 pub mod description_menu;
 pub mod dialog;
 pub mod dropdown;
 pub mod file_explorer;
+pub mod footer;
+pub mod form_field;
+pub mod form_page;
+pub mod layout;
 pub mod progress_bar;
 pub mod selectable_list;
-// Re-exports :D
+pub mod text_input;
+pub mod toggle;
 
+pub use activity_indicator::{ActivityExt, ActivityIndicator, Badge};
 pub use blinking_stars::Stars;
 pub use description_menu::{DescriptionMenu, DescriptionMenuItem};
 pub use dialog::{DialogBuilder, DialogButton};
 pub use dropdown::{Dropdown, DropdownOption};
 pub use file_explorer::{ExplorerResult, FileExplorer};
+pub use form_field::FormField;
+pub use form_page::{FormItem, FormPage, FormSection};
+pub use layout::RectExt;
 pub use progress_bar::ProgressBar;
 use ratatui::buffer::Buffer;
 use ratatui::crossterm::event::KeyEvent;
 use ratatui::layout::Rect;
+pub use text_input::TextInput;
+pub use toggle::Toggle;
 
 use crate::app::AppCtx;
 use crate::themes::Theme;
