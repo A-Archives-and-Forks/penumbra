1f515f04088c247661c7ecfa0dfd73dc26e5bf7b
shomy <git@itssho.my>
2026-08-26 13:49:08 +0200

    core: Ditch DA parser in favour of `hacc` one
    Signed-off-by: shomy <git@itssho.my>

--- core/src/da/mod.rs
+++ core/src/da/mod.rs
@@ -2,11 +2,9 @@
     SPDX-License-Identifier: AGPL-3.0-or-later
     SPDX-FileCopyrightText: 2025-2026 Shomy
 */
-pub mod dafile;
 pub mod protocol;
 pub mod xflash;
 pub mod xml;
-pub use dafile::{DA, DAEntryRegion, DAFile, DAType};
 pub use protocol::{DAProtocol, DAProtocolParams, DownloadProtocol};
 pub use xflash::XFlash;
 pub use xml::Xml;
