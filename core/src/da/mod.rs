d8dba635420404d86cbf295e2d91dfa282e15ec8
shomy <git@itssho.my>
2026-08-26 13:51:14 +0200

    core: Add scatter file parser
    Signed-off-by: shomy <git@itssho.my>

--- core/src/da/mod.rs
+++ core/src/da/mod.rs
@@ -3,8 +3,10 @@
     SPDX-FileCopyrightText: 2025-2026 Shomy
 */
 pub mod protocol;
+mod scatter;
 pub mod xflash;
 pub mod xml;
 pub use protocol::{DAProtocol, DAProtocolParams, DownloadProtocol};
+pub use scatter::{ScatterFile, ScatterOp, ScatterPartition};
 pub use xflash::XFlash;
 pub use xml::Xml;
